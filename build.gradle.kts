164a33f7e2cdff4cf1b8fdbe166b79978c63b5f0
Anastasiia Makarenko <irishmoth@yandex.ru>
2025-02-14 23:35:04 +0300

    Переделываю на котлин-библиотеку

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("java-library")
+    id("org.jetbrains.kotlin.jvm") version "1.9.0"
+}
+
+repositories {
+    mavenCentral()  // Указываем Maven Central
+    // jcenter()  // Если необходимо, можно добавить jcenter
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_7
+    targetCompatibility = JavaVersion.VERSION_1_7
+}
---
+++ src/main/java/pro/progr/diamondapi/DiamondInterface.kt
@@ -0,0 +1,4 @@
+package pro.progr.diamondapi
+
+class DiamondInterface {
+}
