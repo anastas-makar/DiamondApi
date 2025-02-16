1299bdd9e543b0c19c789a0e916ae92ea3d1115d
Anastasiia Makarenko <irishmoth@yandex.ru>
2025-02-16 17:54:53 +0300

    Настройки для публикации

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 plugins {
     id("java-library")
     id("org.jetbrains.kotlin.jvm") version "1.9.0"
+    `maven-publish`
 }
 
 repositories {
     mavenCentral()  // Указываем Maven Central
+    maven { url = uri("https://maven.pkg.github.com/anastas-makar/DiamondApi") }
     // jcenter()  // Если необходимо, можно добавить jcenter
 }
 
@@ -15,4 +17,28 @@ java {
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")  // для корутин
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            groupId = "pro.progr"
+            artifactId = "diamond-api"
+            version = "0.0.1-alpha"
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/anastas-makar/DiamondApi")
+
+            credentials {
+                username = (project.findProperty("gpr.user") ?: System.getenv("GITHUB_USERNAME")).toString()
+                password = (project.findProperty("gpr.token") ?: System.getenv("GITHUB_TOKEN")).toString()
+            }
+        }
+    }
 }
