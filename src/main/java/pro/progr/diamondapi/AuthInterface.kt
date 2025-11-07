50897e5d81482b0f809f595ee1d0546c7a855fff
Anastasiia Makarenko <irishmoth@yandex.ru>
2025-11-07 18:13:48 +0300

    Дорабатываю AuthInterface

--- src/main/java/pro/progr/diamondapi/AuthInterface.kt
+++ src/main/java/pro/progr/diamondapi/AuthInterface.kt
@@ -5,9 +5,27 @@ import kotlinx.coroutines.flow.Flow
 interface AuthInterface {
     fun getName() : String?
 
+    fun setName(name : String?)
+
     fun getSessionId() : String?
 
-    fun getHash(bytes: ByteArray) : String
+    fun setSessionId(sessionId : String)
+
+    fun getSessionSecret() : String?
+
+    /**
+     * Секрет, который сохраняется в зашифрованном виде один раз при авторизации
+     */
+    fun setSessionSecret(sessionSecret : String)
+
+    fun clearSession()
+
+    fun getNonce() : String
+
+    fun getHash(sessionId: String,
+                deviceId: String,
+                nonce: String,
+                bodyBytes: ByteArray) : String
 
     fun getDeviceId() : String
 
